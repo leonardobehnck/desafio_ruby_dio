6e51bb08f3cfe8a45fd2774299fd24911787bc27
Leonardo Behnck <leonardo.behnck@gmail.com>
2024-03-23 17:49:37 -0300

    primeiro commit

---
+++ init.rb
@@ -0,0 +1,10 @@
+print "Digite seu nome: "
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+print "Digite sua idade: "
+idade = gets.chomp
+
+puts "Olá, #{nome} #{sobrenome} você tem #{idade} anos!"
